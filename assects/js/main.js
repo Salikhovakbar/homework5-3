a2f2a609e597c7db7f64a77139b6ab67838ee7ec
Salikhovakbar <pcfjkjhkk@gmail.com>
2022-09-06 15:34:05 +0500

    first commit

---
+++ assects/js/main.js
@@ -0,0 +1,15 @@
+alert ("Welcome");
+var userName = prompt ("Enter your Name");
+console.log(userName);
+var userSurname = prompt (userName + " enter your surname");
+console.log(userSurname);
+var userAge = prompt ("Enter your age");
+console.log(userAge);
+var userAddress = prompt ("Enter your address");
+console.log(userAddress);
+confirm ("Do you wish to login");
+console.log(userName + " " + userAge + " " + userAddress );
+btn.addEventListener('click', () => {
+userYear = 2022;
+    console.log(userYear - userAge)
+})
